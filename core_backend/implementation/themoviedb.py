06b6bafdb953f02036479faec0ca2c7082f0c8f3
jephthah-snr <60290652+jephthah-snr@users.noreply.github.com>
2024-01-15 02:37:12 +0100

    feat: impemented .env for secure data access

--- core_backend/implementation/themoviedb.py
+++ core_backend/implementation/themoviedb.py
@@ -1,11 +1,12 @@
 import requests
+import os
 
 def get_movie():
-    url = "https://api.themoviedb.org/3/person/popular?language=en-US&page=1"
+    url = os.getenv('THEMOVIE_DB_BASE_URL', default='https://api.themoviedb.org/3/person/popular?language=en-US&page=1')
 
     headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTIzYzFkODMxMTFiZmM0ZjE3YTkxYTI4YmY2MGI4YiIsInN1YiI6IjY1YTJlMWQ5YTZhNGMxMDEzMWE0NzhlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jXjzoyeIEmFlN6JBFkLloT9QFxT2msE-cgoX42A1If8"
+    "Authorization": f"Bearer {os.getenv('THEMOVIE_DB_AUTH')}"
     }
 
     response = requests.get(url, headers=headers)
--- lazone_api_service/wsgi.py
+++ lazone_api_service/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+
 import os
+from dotenv import load_dotenv
+
+# Loading environment variables from .env
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
