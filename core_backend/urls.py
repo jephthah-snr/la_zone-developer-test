567b4b0795280b291b5384d582c56b10dc82330c
jephthah-snr <60290652+jephthah-snr@users.noreply.github.com>
2024-01-16 19:58:28 +0100

    fixed database postgresql connection srror

--- core_backend/urls.py
+++ core_backend/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.start_game, name='start_game'),
-    path('/<str:hash>/play', views.play_game, name='play_game'),
-    path('/<str:hash>/play/submit', views.submit_answer, name='submit_answer'),
-    path('/<str:hash>', views.get_score, name='submit_answer'),
-    path('/health/readyz', views.health_api)
+    path('', views.start_game, name='start_game'),
+    path('<str:hash>/play', views.play_game, name='play_game'),
+    path('<str:hash>/play/submit', views.submit_answer, name='submit_answer'),
+    path('<str:hash>', views.get_score, name='submit_answer'),
+    path('health/readyz', views.health_api)
 ]
